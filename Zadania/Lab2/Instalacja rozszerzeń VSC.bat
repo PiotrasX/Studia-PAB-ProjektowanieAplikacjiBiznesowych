35a5ff828eef1f53f7260ce0b5991d1afe27e871
Piotr Rojek <128423791+PiotrasX@users.noreply.github.com>
2025-01-28 10:33:50 +0100

    Add files via upload

---
+++ Zadania/Lab2/Instalacja rozszerzeń VSC.bat
@@ -0,0 +1,11 @@
+call code --install-extension onecentlin.laravel-extension-pack      --force 
+
+call code --install-extension xdebug.php-debug                       --force 
+
+call code --install-extension bmewburn.vscode-intelephense-client    --force 
+
+call code --install-extension humao.rest-client                      --force 
+
+call code --install-extension pkief.material-icon-theme              --force 
+
+pause
